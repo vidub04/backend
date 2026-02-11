6d954e1e1bc98ae1a561c21662b8f0d5f42ae7fc
unknown <vidub04@gmail.com>
2026-02-11 21:02:01 +0530

    os env

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("postgresql://attendance_db_93y7_user:yy7jHyEoEuV5z4B8JcdpGajMzjQSCITm@dpg-d668tmv5r7bs73cdmtlg-a/attendance_db_93y7")
+DATABASE_URL = os.getenv("DATABASE_URL")  # <-- THIS ONLY NAMES THE VARIABLE
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not set in environment!")
