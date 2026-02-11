0dc314f6306547b9e14594309f242743d47e9684
unknown <vidub04@gmail.com>
2026-02-11 19:49:57 +0530

    switched to postgre

--- app/database.py
+++ app/database.py
@@ -1,12 +1,10 @@
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./attendance.db"
+DATABASE_URL = os.getenv("postgresql://attendance_db_93y7_user:yy7jHyEoEuV5z4B8JcdpGajMzjQSCITm@dpg-d668tmv5r7bs73cdmtlg-a/attendance_db_93y7")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
