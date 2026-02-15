e58a656b778ebbe5850c97136afa2b08a8c32f00
unknown <vidub04@gmail.com>
2026-02-15 11:22:08 +0530

    added cors api

--- app/main.py
+++ app/main.py
@@ -11,6 +11,16 @@ from app import models
 #Base.metadata.create_all(bind=engine)
 ##print("Tables recreated")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 
 app = FastAPI()
